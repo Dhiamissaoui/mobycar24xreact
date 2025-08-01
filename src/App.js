ec90d3e99ec691629212e4051d4c7fd5add784fe
Dhiamissaoui <dhiamissaoui17@gmail.com>
2025-08-01 10:12:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RentCar from './pages/RentCar';
 import Brand from './pages/Brand'; // You'll need to create this
 import Login from './pages/Login'; // You'll need to create this
 
-import '/mobycar24xreact/src/styles/App.css';
+import './styles/App.css';
 
 function App() {
   return (
